7e5458c342943e9ad5ced2c28d3705a90febaeed
Vikash <vikash0cv@gmail.com>
2023-03-30 10:07:14 +0530

    modified soilType schema for better geoSpatial tasks

--- api/Prediction.js
+++ api/Prediction.js
@@ -1,39 +1,60 @@
 const express = require('express');
 const SoilType = require('./../models/SoilType.js');
 const router = express.Router();
-
+const geolib = require('geolib');
 // mongodb Predictions model
-const User = require('./../models/SoilType.js');
+// const User = require('./../models/SoilType.js');
 
-router.post('/save', (req, res) => {
-    console.log(req.body);
-    let userId = req.body.userId;
-    let latitude = req.body.latitude;
-    let longitude = req.body.longitude;
-    let prediction = req.body.prediction;
-    prediction = prediction.trim();
-    const newSoilType = new SoilType({
-        userId,
-        latitude,
-        longitude,
-        soilType: prediction,
-    });
-    newSoilType.save().then(result => {
-        res.status(200).json({
-            status: "SUCCESS",
-            message: "Save Successfull",
-            data: {
-                result
+router.post('/save', async (req, res) => {
+    try {
+        const { userId, prediction, latitude, longitude } = req.body;
+
+        // Create a new soil type object
+        const newSoilType = new SoilType({
+            userId,
+            soilType: prediction,
+            location: {
+                type: 'Point',
+                coordinates: [longitude, latitude],
             },
-        })
-    })
-        .catch(err => {
-            res.status(500).json({
-                status: "FAILED",
-                message: "An error occurred while saving prediction.",
-                details: err,
-            })
-        })
-})
+        });
+
+        // Save the new soil type object to the database
+        const savedSoilType = await newSoilType.save();
+
+        res.status(201).json(savedSoilType);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+
+router.post('/search', async (req, res) => {
+    const { latitude, longitude, radius } = req.body;
+
+    try {
+        const nearbySoilTypes = await SoilType.find({
+            location: {
+                $geoWithin: {
+                    $centerSphere: [
+                        [longitude, latitude],
+                        radius / 6371 // 6371 km is the radius of the Earth
+                    ]
+                }
+            }
+        });
+        const count = nearbySoilTypes.length;
+        res.json({
+            count: count,
+            data: nearbySoilTypes
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
 
 module.exports = router;
